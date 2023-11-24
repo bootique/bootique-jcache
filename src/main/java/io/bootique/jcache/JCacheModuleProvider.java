35c192346c5c7408cdc5ced51cfd090b72b76884
Andrus Adamchik <andrus@objectstyle.com>
2023-11-24 15:01:40 -0500

    Upgrading to the latest BQModuleProvider format

--- src/main/java/io/bootique/jcache/JCacheModuleProvider.java
+++ src/main/java/io/bootique/jcache/JCacheModuleProvider.java
@@ -19,33 +19,18 @@
 
 package io.bootique.jcache;
 
-import io.bootique.BQModuleMetadata;
 import io.bootique.BQModuleProvider;
-import io.bootique.di.BQModule;
-
-import java.lang.reflect.Type;
-import java.util.Collections;
-import java.util.Map;
+import io.bootique.bootstrap.BuiltModule;
 
 public class JCacheModuleProvider implements BQModuleProvider {
 
     @Override
-    public BQModule module() {
-        return new JCacheModule();
-    }
-
-    @Override
-    public Map<String, Type> configs() {
-        // TODO: config prefix is hardcoded. Refactor away from ConfigModule, and make provider
-        // generate config prefix, reusing it in metadata...
-        return Collections.singletonMap("jcache", JCacheFactory.class);
-    }
-
-    @Override
-    public BQModuleMetadata.Builder moduleBuilder() {
-        return BQModuleProvider.super
-                .moduleBuilder()
-                .description("Provides configuration for JCache subsystem. Module itself does NOT include a JCache provider."
-                        + " The users will need to add a provider of their choice to the application classpath.");
+    public BuiltModule buildModule() {
+        return BuiltModule.of(new JCacheModule())
+                .provider(this)
+                .description("Integrates configuration for the JCache subsystem. Module itself does NOT include a JCache " +
+                        "provider. Users will need to add a provider of their choice to the application classpath.")
+                .config("jcache", JCacheFactory.class)
+                .build();
     }
 }
